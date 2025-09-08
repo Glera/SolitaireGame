fc99ffc2f1c28ccc75a4497de4bceeba4befd8cc
glerateam <46315412-glerateam@users.noreply.replit.com>
2025-09-08 13:35:31 +0000

    Restored to 'fde5723b847785da964825891ef5e780e37e586b'
    Replit-Restored-To: fde5723b847785da964825891ef5e780e37e586b

--- client/src/components/solitaire/GameControls.tsx
+++ client/src/components/solitaire/GameControls.tsx
@@ -3,7 +3,7 @@ import { useSolitaire } from '../../lib/stores/useSolitaire';
 import { Button } from '../ui/button';
 import { Card } from '../ui/card';
 
-const GAME_VERSION = '1.2.1';
+const GAME_VERSION = '1.2.0';
 
 interface GameControlsProps {
   onDebugClick?: () => void;
